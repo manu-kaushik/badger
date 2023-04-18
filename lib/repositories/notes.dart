82f12df521ecfe8c8018ba1c0ce39edbc58ed395
BitterByter <manukaushik.m@gmail.com>
2023-04-18 19:25:15 +0530

    Add notes and todos repositories - Add dependencies sqflite, path

---
+++ lib/repositories/notes.dart
@@ -0,0 +1,82 @@
+import 'dart:async';
+
+import 'package:notes/models/note.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class NotesRepository {
+  static final NotesRepository _singleton = NotesRepository._internal();
+
+  static const String _dbName = 'notes.db';
+  static const int _dbVersion = 1;
+
+  static const String _tableName = 'notes';
+  static const String _colId = 'id';
+  static const String _colTitle = 'title';
+  static const String _colBody = 'body';
+
+  late Database _database;
+
+  NotesRepository._internal();
+
+  factory NotesRepository() {
+    return _singleton;
+  }
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database;
+    } else {
+      _database = await _initDatabase();
+
+      return _database;
+    }
+  }
+
+  Future<Database> _initDatabase() async {
+    final String dbPath = await getDatabasesPath();
+
+    final String path = join(dbPath, _dbName);
+
+    return await openDatabase(path, version: _dbVersion,
+        onCreate: (Database db, int version) async {
+      await db.execute('''
+          CREATE TABLE $_tableName (
+            $_colId INTEGER PRIMARY KEY AUTOINCREMENT,
+            $_colTitle TEXT,
+            $_colBody TEXT
+          )
+          ''');
+    });
+  }
+
+  Future<void> insert(Note note) async {
+    final db = await database;
+
+    await db.insert(_tableName, note.toJson(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  Future<List<Note>> getAll() async {
+    final db = await database;
+
+    final List<Map<String, dynamic>> maps = await db.query(_tableName);
+
+    return List.generate(maps.length, (i) {
+      return Note.fromJson(maps[i]);
+    });
+  }
+
+  Future<void> update(Note note) async {
+    final db = await database;
+
+    await db.update(_tableName, note.toJson(),
+        where: '$_colId = ?', whereArgs: [note.id]);
+  }
+
+  Future<void> delete(Note note) async {
+    final db = await database;
+
+    await db.delete(_tableName, where: '$_colId = ?', whereArgs: [note.id]);
+  }
+}
---
+++ lib/repositories/todos.dart
@@ -0,0 +1,95 @@
+import 'dart:async';
+
+import 'package:notes/models/todo.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class TodosRepository {
+  static final TodosRepository _singleton = TodosRepository._internal();
+
+  static const String _dbName = 'todos.db';
+  static const int _dbVersion = 1;
+
+  static const String _tableName = 'todos';
+  static const String _colId = "id";
+  static const String _colText = "text";
+  static const String _colCompleted = "completed";
+  static const String _colColor = "color";
+
+  late Database _database;
+
+  TodosRepository._internal();
+
+  factory TodosRepository() {
+    return _singleton;
+  }
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database;
+    } else {
+      _database = await _initDatabase();
+
+      return _database;
+    }
+  }
+
+  Future<Database> _initDatabase() async {
+    final String dbPath = await getDatabasesPath();
+
+    final String path = join(dbPath, _dbName);
+
+    return await openDatabase(path, version: _dbVersion,
+        onCreate: (Database db, int version) async {
+      await db.execute('''
+          CREATE TABLE $_tableName(
+            $_colId INTEGER PRIMARY KEY, 
+            $_colText TEXT, 
+            $_colCompleted INTEGER, 
+            $_colColor TEXT
+          )
+          ''');
+    });
+  }
+
+  Future<void> insert(Todo todo) async {
+    final db = await database;
+
+    await db.insert(
+      _tableName,
+      todo.toJson(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Todo>> getAll() async {
+    final db = await database;
+
+    final List<Map<String, dynamic>> maps = await db.query(_tableName);
+
+    return List.generate(maps.length, (i) {
+      return Todo.fromJson(maps[i]);
+    });
+  }
+
+  Future<void> update(Todo todo) async {
+    final db = await database;
+
+    await db.update(
+      _tableName,
+      todo.toJson(),
+      where: "$_colId = ?",
+      whereArgs: [todo.id],
+    );
+  }
+
+  Future<void> delete(Todo todo) async {
+    final db = await database;
+
+    await db.delete(
+      _tableName,
+      where: "$_colId = ?",
+      whereArgs: [todo.id],
+    );
+  }
+}
