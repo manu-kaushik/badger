5770f52abca9372409da12de76e1ba8348418fd0
BitterByter <manukaushik.m@gmail.com>
2023-05-05 16:00:07 +0530

    Add custom app colors and fonts

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:material_color_generator/material_color_generator.dart';
 
-const primaryColor = Colors.blue;
+MaterialColor primaryColor =
+    generateMaterialColor(color: const Color(0xFF004FFF));
+MaterialColor secondaryColor =
+    generateMaterialColor(color: const Color(0xFFF18701));
+MaterialColor accentColor =
+    generateMaterialColor(color: const Color(0xFFD6E3F8));
+MaterialColor darkColor = generateMaterialColor(color: const Color(0xFF30323D));
--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:notes/utils/colors.dart';
 
 ThemeData theme = ThemeData(
+  primaryColor: primaryColor,
   primarySwatch: primaryColor,
+  fontFamily: GoogleFonts.quicksand(fontWeight: FontWeight.w500).fontFamily,
 );
